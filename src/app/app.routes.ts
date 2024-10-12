ec16f403f17124aa897e0e64752e8d37ad7fd181
Erfan Nikaan <erfan2004nikan@gmail.com>
2024-10-12 11:28:34 +0330

    chore: some comment...
    Signed-off-by: Erfan Nikaan <erfan2004nikan@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-      },
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'login',
     component: LoginComponent,
